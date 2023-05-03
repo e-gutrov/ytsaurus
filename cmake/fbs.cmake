6484a1e312c654c6c7bb73e3ebf8defd9f43dba2
Egor Gutrov <gutrov.egor@gmail.com>
2023-05-03 22:40:31 +0100

    Fixed CMake

--- cmake/fbs.cmake
+++ cmake/fbs.cmake
@@ -3,27 +3,27 @@ include(common)
 function(target_fbs_source Tgt Key Src)
     get_built_tool_path(flatc_bin flatc_dependency contrib/tools/flatc/bin  flatc)
 
-    file(RELATIVE_PATH fbsRel ${CMAKE_SOURCE_DIR} ${Src})
+    file(RELATIVE_PATH fbsRel ${YT_SOURCE_DIR} ${Src})
     get_filename_component(OutputBase ${fbsRel} NAME_WLE)
-    get_filename_component(OutputDir ${CMAKE_BINARY_DIR}/${fbsRel} DIRECTORY)
+    get_filename_component(OutputDir ${YT_BINARY_DIR}/${fbsRel} DIRECTORY)
     add_custom_command(
       OUTPUT
-        ${CMAKE_BINARY_DIR}/${fbsRel}.h
-        ${CMAKE_BINARY_DIR}/${fbsRel}.cpp
+        ${YT_BINARY_DIR}/${fbsRel}.h
+        ${YT_BINARY_DIR}/${fbsRel}.cpp
         ${OutputDir}/${OutputBase}.iter.fbs.h
         ${OutputDir}/${OutputBase}.bfbs
       COMMAND Python3::Interpreter
-        ${CMAKE_SOURCE_DIR}/build/scripts/cpp_flatc_wrapper.py
+        ${YT_SOURCE_DIR}/build/scripts/cpp_flatc_wrapper.py
         ${flatc_bin}
         ${FBS_CPP_FLAGS} ${ARGN}
-        -o ${CMAKE_BINARY_DIR}/${fbsRel}.h
+        -o ${YT_BINARY_DIR}/${fbsRel}.h
         ${Src}
-      DEPENDS ${CMAKE_SOURCE_DIR}/build/scripts/cpp_flatc_wrapper.py ${Src} ${flatc_dependency}
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+      DEPENDS ${YT_SOURCE_DIR}/build/scripts/cpp_flatc_wrapper.py ${Src} ${flatc_dependency}
+      WORKING_DIRECTORY ${YT_BINARY_DIR}
     )
     target_sources(${Tgt} ${Key}
-      ${CMAKE_BINARY_DIR}/${fbsRel}.cpp
-      ${CMAKE_BINARY_DIR}/${fbsRel}.h
+      ${YT_BINARY_DIR}/${fbsRel}.cpp
+      ${YT_BINARY_DIR}/${fbsRel}.h
       ${OutputDir}/${OutputBase}.iter.fbs.h
     )
 endfunction()
--- contrib/libs/llvm12/include/epilogue.cmake
+++ contrib/libs/llvm12/include/epilogue.cmake
@@ -1,3 +1,3 @@
 add_custom_target(OpenMP-gen-srcs-stealing
-  DEPENDS ${CMAKE_BINARY_DIR}/contrib/libs/llvm12/lib/Frontend/OpenMP/OMP.cpp
+  DEPENDS ${YT_BINARY_DIR}/contrib/libs/llvm12/lib/Frontend/OpenMP/OMP.cpp
 )
